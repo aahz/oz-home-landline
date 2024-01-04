9f7ca7d3fc9690b56633c2c3a4cf026a3ea2a41e
Andrei Ozdon <aahzzyy@gmail.com>
2024-01-04 22:22:50 +0300

    Some minor fixes.

--- src/constants/modem.ts
+++ src/constants/modem.ts
@@ -1,6 +1,4 @@
 import {getEnv} from "../utility";
 
-export const PATH = String(getEnv('MODEM_PATH', '/dev/modem')) as string;
+export const PATH = String(getEnv('MODEM_PATH', '/dev/ttyACM0')) as string;
 export const BAUD_RATE = parseInt(getEnv('MODEM_BAUD_RATE', 115200), 10) as number;
-
-export const COMMANDS_SEQUENCE = String(getEnv('MODEM_COMMANDS_SEQUENCE', 'Z;+FCLASS=8;L1;A;H;L3;m1x3DT{{PHONE_NUMBER}}'));
