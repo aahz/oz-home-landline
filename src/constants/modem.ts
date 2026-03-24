27eeb51a2ecdd8ce5a2f788dbbf8dfdd6df7b3f5
Andrei Ozdon <aahzzyy@gmail.com>
2026-03-24 17:00:00 +0300

    feat(modem): add fallback API config constants

--- src/constants/modem.ts
+++ src/constants/modem.ts
@@ -2,3 +2,6 @@ import {getEnv} from "../utility";
 
 export const PATH = String(getEnv('MODEM_PATH', '/dev/ttyACM0')) as string;
 export const BAUD_RATE = parseInt(getEnv('MODEM_BAUD_RATE', 9600), 10) as number;
+
+export const FALLBACK_API_PATH = String(getEnv('MODEM_FALLBACK_API_PATH', 'https://modem.ozdon.online/api/v1')) as string;
+export const FALLBACK_API_TOKEN = String(getEnv('MODEM_FALLBACK_API_TOKEN', '')) as string;
